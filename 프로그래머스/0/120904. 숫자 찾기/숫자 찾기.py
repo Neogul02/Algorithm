8d228f6b708d4ab6fdd26f8116fec04a66ec1a0a
Choe JinHyeong <111578210+Neogul02@users.noreply.github.com>
2024-04-20 23:57:23 +0900

    [level 0] Title: 숫자 찾기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.py
@@ -0,0 +1,12 @@
+def solution(num, k):
+
+    num = list(str(num))
+
+    for i in num:
+        if i == str(k):
+            index = num.index(i)+1
+            break
+        else:
+            index = -1
+
+    return index
