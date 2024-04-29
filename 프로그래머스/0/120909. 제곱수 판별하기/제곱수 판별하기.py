0bc45a613be047ba1d1068e107f3b815d6fe410a
Choe JinHyeong <111578210+Neogul02@users.noreply.github.com>
2024-04-30 00:01:52 +0900

    Update 제곱수 판별하기.py

--- 프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.py
+++ 프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.py
@@ -1,9 +1,3 @@
 def solution(n):
-    answer = 0
-    for i in range(1, 1000001):
-        if n == i**2:
-            answer = 1
-            break
-        else:
-            answer = 2
-    return answer
+    root = n ** 0.5
+    return 1 if root == int(root) else 2
