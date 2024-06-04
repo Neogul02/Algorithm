f6a3217e882007a1bac3f0ff49b5f3cde55e2f6d
Choe JinHyeong <111578210+Neogul02@users.noreply.github.com>
2024-06-04 22:31:40 +0900

    [level 0] Title: 연속된 수의 합, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120923. 연속된 수의 합/연속된 수의 합.py
@@ -0,0 +1,8 @@
+def solution(num, total):
+    d = 0
+    for i in range(1, num):
+        d += i
+
+    start = (total-d) // num
+    answer = [i for i in range(start, start+num)]
+    return answer
