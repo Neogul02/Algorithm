f29b4b789bee97e7b50db0601209e24bc2b40a36
Choe JinHyeong <111578210+Neogul02@users.noreply.github.com>
2024-07-01 18:43:06 +0900

    [level 0] Title: 수 조작하기 1, Time: 3.79 ms, Memory: 36.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.js
@@ -0,0 +1,9 @@
+function solution(n, control) {
+    for(let i=0; i<control.length; i++){
+        if (control[i] == 'w') n += 1
+        if (control[i] == 's') n -= 1
+        if (control[i] == 'd') n += 10
+        if (control[i] == 'a') n -= 10
+    }
+    return n;
+}
