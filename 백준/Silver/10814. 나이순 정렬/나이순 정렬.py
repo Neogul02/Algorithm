0b2aefc6da1e2f83196f9f603bd0dfa733e491fd
Choe JinHyeong <wlsgud2414@kangnam.ac.kr>
2024-11-25 22:24:47 +0900

    [Silver V] Title: 나이순 정렬, Time: 2752 ms, Memory: 46140 KB -BaekjoonHub

---
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.py
@@ -0,0 +1,11 @@
+N = int(input())
+arr = []
+
+for _ in range(N):
+    age, name = input().split()
+    arr.append((int(age), name))
+
+arr.sort(key=lambda x: x[0])
+
+for age, name in arr:
+    print(age, name)
