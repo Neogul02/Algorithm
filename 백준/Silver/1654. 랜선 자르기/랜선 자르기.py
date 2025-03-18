10f06e2504b39cb330555289830bfb12bee17e3c
Choe JinHyeong <wlsgud2414@kangnam.ac.kr>
2025-03-18 14:17:21 +0900

    [Silver II] Title: 랜선 자르기, Time: 84 ms, Memory: 33432 KB -BaekjoonHub

---
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
@@ -0,0 +1,18 @@
+import sys
+
+K, N = map(int, input().split())
+lan = [int(sys.stdin.readline()) for _ in range(K)]
+start = 1
+end = max(lan)
+
+while start <= end:
+    mid = (start + end) // 2  # 중간 찾기
+    lines = 0
+    for i in lan:
+        lines += i // mid
+
+    if lines >= N:
+        start = mid + 1
+    else:
+        end = mid - 1
+print(end)
