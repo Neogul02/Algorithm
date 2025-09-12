760d5951f8173c59c1a653f794eae17f294389e6
Choe JinHyeong <wlsgud2414@kangnam.ac.kr>
2025-09-12 14:42:01 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 232 ms, Memory: 51528 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.py
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.py
@@ -1,7 +1,11 @@
+import sys
+input = sys.stdin.readline
+
 N, M = map(int, input().split())
 
 num_to_name = {}
 name_to_num = {}
+
 for i in range(1, N + 1):
     name = input().strip()
     num_to_name[i] = name
