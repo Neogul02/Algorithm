82c6c8abb53baf308647565e1263b0efb4151207
Choe JinHyeong <wlsgud2414@kangnam.ac.kr>
2025-10-09 11:33:50 +0900

    [Bronze I] Title: 2진수 뒤집기, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/11179. 2진수 뒤집기/2진수 뒤집기.py
+++ 백준/Bronze/11179. 2진수 뒤집기/2진수 뒤집기.py
@@ -1,6 +1,1 @@
-N = format(int(input()), 'b')
-reversed_N = ''
-for i in range(1,len(N)+1):
-    reversed_N = reversed_N + N[-i]
-
-print(int(reversed_N, 2))
+print(int(format(int(input()), 'b')[::-1],2))
