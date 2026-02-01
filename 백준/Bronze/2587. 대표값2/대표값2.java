16cb2ef010a59f8204447a0f674f810adda26264
Choe JinHyeong <wlsgud2414@kangnam.ac.kr>
2026-02-01 22:56:13 +0900

    [Bronze II] Title: 대표값2, Time: 180 ms, Memory: 17696 KB -BaekjoonHub

---
+++ 백준/Bronze/2587. 대표값2/대표값2.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int[] arr = new int[5];
+		for(int i =0; i<5; i++) {
+			arr[i] = sc.nextInt();
+		}
+		Arrays.sort(arr);
+		System.out.println(Arrays.stream(arr).sum()/5);
+		System.out.print(arr[2]);
+	}
+}
