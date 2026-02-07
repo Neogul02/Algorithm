d38578c5b4de31e55d3ec5dfc29ea54f70135c8a
Choe JinHyeong <wlsgud2414@kangnam.ac.kr>
2026-02-07 11:01:08 +0900

    [D2] Title: 새로운 불면증 치료법, Time: 106 ms, Memory: 25,216 KB -BaekjoonHub

---
+++ SWEA/D2/1288. 새로운 불면증 치료법/새로운 불면증 치료법.java
@@ -0,0 +1,52 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * 1288. 새로운 불면증 치료법 [D2]
+ * @author neogul02
+ */
+public class Solution {
+	
+	static BufferedReader br;
+	static StringBuilder sb = new StringBuilder();
+	
+	static int N;
+	static int cnt;
+	
+	public static void main(String[] args) throws IOException{
+		br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int T = Integer.parseInt(br.readLine().trim());
+		for(int tc=1; tc<=T; tc++) {
+			
+			N = Integer.parseInt(br.readLine().trim());
+			int result = countSheep();
+			
+			sb.append("#").append(tc).append(" ")
+			.append(result).append("\n");
+		}
+		System.out.print(sb);
+	}
+
+	public static int countSheep() {
+		int visited = 0; // 기록
+		int cnt = 0;
+		
+		int k=1;
+		
+		while(true) {
+			if(visited==(1<<10)-1) break;
+			cnt = N * k;
+			int temp = cnt;
+			
+			while (temp > 0) {
+			    int digit = temp % 10; // 1. 나머지 연산으로 맨 끝자리 획득
+			    visited |= (1 << digit);
+			    temp /= 10;            // 2. 몫 연산으로 맨 끝자리 제거
+			}
+			k++;
+		}
+		return cnt;
+	}
+}
