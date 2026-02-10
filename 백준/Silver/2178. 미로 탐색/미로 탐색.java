fea8d92066435d6a9da73176960d9cc943a47a74
Choe JinHyeong <wlsgud2414@kangnam.ac.kr>
2026-02-10 10:15:28 +0900

    [Silver I] Title: 미로 탐색, Time: 64 ms, Memory: 12044 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -25,49 +25,45 @@ public class Main {
 			char[] tempArr = br.readLine().trim().toCharArray();
 			map[i] = tempArr;
 		}
-		
-		bfs();
+		bfs();	
 	}
 
 	public static void bfs() {
-		int[] dx = {-1,1,0,0}; // 1. 델타배열을 만든다
+		// 델타 배열을 만들어보자
+		int[] dx = {-1,1,0,0};
 		int[] dy = {0,0,-1,1};
 		
-		// 2. 큐에 초기 위치를 넣어둔다
-		ArrayDeque<int[]> queue = new ArrayDeque<>(); 
-		queue.add(new int[] {0,0,1}); // 시작점이랑 현재 거리 저장
-		
-		// 탐색한 위치는 다시 돌아가지 않도록 벽으로 처리
+		// 큐 하나를 생성해 초기 위치를 잡아둔다.
+		ArrayDeque<int[]> queue = new ArrayDeque<>();
+		queue.add(new int[] {0,0,1});
 		map[0][0] = '0';
 		
-		while(!queue.isEmpty()) { // 큐가 비어있지 않을때까지 반복 
+		
+		// 큐가 비어있지 않을때까지 반복
+		while(!queue.isEmpty()) {
+			int [] temp = queue.poll();
 			
-			int [] current = queue.poll(); // 큐 맨앞 값을 가져오고 cuurent로 저장
+			int x = temp[0];
+			int y = temp[1];
+			int depth = temp[2];
 			
-			// current에서 뽑아서 가져옴
-			int x = current[0];
-			int y = current[1];
-			int depth = current[2];
-			
-			// 상하좌우 델타 좌표롤 검사
 			for(int i=0; i<4; i++) {
-				int nx = x+dx[i];
-				int ny = y+dy[i];
-				
-				// map index 안의 위치해있으면 
+				int nx = x+ dx[i];
+				int ny = y+ dy[i];
+				// 인덱스 바운더리 안이면서
 				if(nx>=0 && nx<N && ny>=0 && ny<M) {
-					if(nx ==N-1 && ny == M-1) { // 도착했을때
-						System.out.print(++depth);
+					// 도착지에 도착하면 sysout ++depth
+					if(nx==N-1 && ny==M-1) {
+						System.out.println(++depth); 
 						return;
-					}
-					// 길이면 벽으로 막아두고 한 칸 이동, 깊이 +1 해서 큐에 넣기
-					if(map[nx][ny] =='1') {
-						map[nx][ny] = '0';
-						queue.add(new int[] {nx, ny, depth+1});
+						}
+					
+					if(map[nx][ny] == '1') {
+						map[nx][ny] ='0';
+						queue.add(new int[] {nx,ny,depth+1});
 					}
 				}
-			}
-			
+			}	
 		}
 	}
 
