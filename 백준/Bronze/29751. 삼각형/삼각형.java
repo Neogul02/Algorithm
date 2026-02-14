4b677f67fea6fac214917f6955839074cc8b2280
Choe JinHyeong <wlsgud2414@kangnam.ac.kr>
2026-02-14 13:55:54 +0900

    [Bronze V] Title: 삼각형, Time: 64 ms, Memory: 11888 KB -BaekjoonHub

--- 백준/Bronze/29751. 삼각형/삼각형.java
+++ 백준/Bronze/29751. 삼각형/삼각형.java
@@ -7,9 +7,6 @@ public class Main {
 	public static void main (String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine().trim()," ");
-		StringBuilder sb = new StringBuilder();
-
-		sb.append(String.format("%.1f",(double)(Integer.parseInt(st.nextToken())*Integer.parseInt(st.nextToken()))/2));
-		System.out.print(sb);
+		System.out.printf("%.1f",(double)(Integer.parseInt(st.nextToken())*Integer.parseInt(st.nextToken()))/2);
     }
 }
