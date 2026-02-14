fde2b6f60e8f4c8ad5d64739d973469d3314394d
Choe JinHyeong <wlsgud2414@kangnam.ac.kr>
2026-02-14 13:23:11 +0900

    [Gold V] Title: Z, Time: 60 ms, Memory: 11504 KB -BaekjoonHub

--- 백준/Gold/1074. Z/Z.java
+++ 백준/Gold/1074. Z/Z.java
@@ -2,27 +2,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
-
-/**
- * 1074. Z
- * @author neogul02
- * 
- * [IDEA]
- * 정사각형 2^N × 2^N 2의 지수N을 입력받아서 배열 초기화,
- * 사각형 사이즈가 2*2 이면 -> 왼쪽위, 오른쪽위, 왼쪽아래, 오른쪽아래 순서로 Z 모양으로 순회
- * 2*2보다 크다면? ex)4*4 라면 4등분해서 왼쪽위 오른쪽위 왼쪽아래 오른쪽아래 순서로 재귀처리 후 위의 로직 반복
- * r c 가 3 1 이면 row 3 col 1 이면 실제 2차원 배열에서는 +1 칸임 4번째행에 2번째 열에 있는 값에 몇번째로 도달하는가?
- * 
- * 1. N r c를 입력받는다.
- * 2. 현재 정사각형의 좌상단 좌표: (row, col) 이라고 할 떄,
- * 	2-1. 위쪽 영역: row ~ row+half-1
- * 	2-2. 아래쪽 영역: row+half ~ row+size-1
- * 	2-3. 왼쪽 영역: col ~ col+half-1
- * 	2-4. 오른쪽 영역: col+half ~ col+size-1
- * 
- * 배열을 실제로 만들지 말고 이동을 추적해볼까
- *
- */
 public class Main {
 	
 	static int N, r, c;
@@ -38,32 +17,33 @@ public class Main {
 		
 		int size = 1<<N; // 2^N == Math.pow(N,2)
 		
-		dfs(0,0,size);
+		dfs(size, 0, 0);
 	}
-    
-	private static void dfs(int row, int col, int size) {
+	private static void dfs(int size, int row, int col) {
+		// 종료조건 찾는 r c 값이라면 
 		 if (size == 1) {
 		        System.out.println(cnt);
 		        return;
 		    }
-        
+		// 수행로직
 		int half = size/2; 
 	    int area = half * half;
+	    
+	    boolean isTop = r<(row+half); // 사각형을 절반으로 나누면 4길이 사각형의 half는 2
+	    boolean isLeft = c<(col+half);
 
-	    if (r < row + half && c < col + half) { // 왼쪽 위
-	        dfs(row, col, half); // 0 부터 시작
-	    } 
-	    else if (r < row + half && c >= col + half) { // 오른쪽 위
-	        cnt += area;
-	        dfs(row, col + half, half);
-	    } 
-	    else if (r >= row + half && c < col + half) { // 왼쪽 아래
-	        cnt += 2 * area;
-	        dfs(row + half, col, half);
-	    } 
-	    else { // 오른쪽 아래
-	        cnt += 3 * area;
-	        dfs(row + half, col + half, half);
-	    }
+	    // z순서로 방문하면 왼쪽 위에서 시작해서 오른쪽 아래가 마지막에 방문
+	     if (isTop && isLeft) {                // 왼쪽 위
+            dfs(half, row, col);
+        } else if (isTop && !isLeft) {        // 오른쪽 위
+            cnt += area;
+            dfs(half, row, col + half);
+        } else if (!isTop && isLeft) {        // 왼쪽 아래
+            cnt += 2 * area;
+            dfs(half, row + half, col);
+        } else {                               // 오른쪽 아래
+            cnt += 3 * area;
+            dfs(half, row + half, col + half);
+        }
 	}
 }
