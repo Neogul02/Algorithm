d9064c57d7066b5d63f91afe6fc884d47317058b
Choe JinHyeong <wlsgud2414@kangnam.ac.kr>
2026-02-18 22:30:38 +0900

    [D4] Title: [S/W 문제해결 기본] 7일차 - 미로1, Time: 80 ms, Memory: 26,112 KB -BaekjoonHub

--- SWEA/D4/1226. ［S／W 문제해결 기본］ 7일차 － 미로1/［S／W 문제해결 기본］ 7일차 － 미로1.java
+++ SWEA/D4/1226. ［S／W 문제해결 기본］ 7일차 － 미로1/［S／W 문제해결 기본］ 7일차 － 미로1.java
@@ -3,74 +3,73 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
 import java.util.Deque;
-import java.util.StringTokenizer;
 
 /**
- * 1226. 미로1
- * 1은 벽, 0은 길, 2는 출발점, 3은 도착
+ * 1226. 미로
+ * @author neogul02
  * 
- * 1. 입력 처리
- *  1-1. 10개로 고정되어있어서 입력만 받기
- *  1-2. 공백 없이 붙어있는 문자열 형태 입력받기
- *  1-3. toCharArray로 2차원배열을 쉽게 만들기
- *  1-4. bfs 함수 만들어서 매개변수로 입력받은 2차원 배열 넘겨주기
- * 2. bfs 함수 구현
- *  2-1. 시작과 끝이 고정되어있으므로 좌표인자없이 배열 map 만 전달
- *  2-2. Deque에 처음 위치를 저장해두고 poll해서 조회하고 현 위치 갱신하면 add
- *  2-3. 델타 탐색 배열 dx, dy 에 4번 반복해서 현위치 기준 상하좌우 탐색
- *  ->? 갈림길이면? if 왼쪽과 오른쪽 모두 길이 있다면, 둘 다 큐에 넣어서 각각 위치 탐색을 예약해두기
- *  2-4. 범위체크, 새로운 인덱스가 배열을 넘어가는지? ndx와 ndy 모두 0과 16의 사이여야함
- *  2-5. '3'을 만나면 return 1 못만나면 return 0
  */
 public class Solution {
 	
-	static BufferedReader br;
-	static StringTokenizer st;
-	static StringBuilder sb;
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));;
+	static char[][] map;; // 16 * 16 행렬
 	
-	public static int bfs(char[][] map) {
-		
-		int[] dx = {-1, 1, 0, 0,};
-		int[] dy = {0, 0, -1, 1};
-		
-		Deque<int[]> queue = new ArrayDeque<>();
+	
+	static int[] dx = {-1,1,0,0};
+	static int[] dy = {0,0,-1,1};
+
+	
+	public static void main(String[] args) throws IOException {
+		StringBuilder sb = new StringBuilder();
+		for(int tc=1; tc<=10; tc++) {
+			
+			input();
+			int result = bfs();
+					
+			sb.append('#').append(tc).append(' ')
+			.append(result).append('\n');
+		}
+		System.out.print(sb);
+	}
+	
+	private static int bfs() {
+		Deque<int[]> queue = new ArrayDeque<int[]>();
 		queue.add(new int[] {1,1});
 		
 		while(!queue.isEmpty()) {
-			int[] current = queue.poll();
-			int x = current[0];
-			int y = current[1];
+			int[] temp = queue.poll();
+			int x = temp[0];
+			int y = temp[1];
 			
 			for(int i=0; i<4; i++) {
-				int ndx = x + dx[i];
-				int ndy = y + dy[i];
+				int nx = dx[i] + x;
+				int ny = dy[i] + y;
 				
-				if(ndx>=0 && ndx<16 && ndy>=0 && ndy<16) {
-					if(map[ndx][ndy] =='3') {
-						return 1;
-					}
-					if(map[ndx][ndy]=='0') {
-						map[ndx][ndy] = '1'; // 현 위치는 벽으로 막기
-						queue.add(new int[] {ndx, ndy});
+				if(nx>=0&&nx<16&&ny>=0&&ny<16) {
+					if(map[nx][ny] == '3') return 1;
+					
+					if(map[nx][ny] == '0') { // 갈 수 있는 곳이면
+						map[nx][ny] = '1'; // 현위치는 방문 처리
+						
+						queue.add(new int[] {nx,ny});
 					}
 				}
 			}
-		}				
-		return 0;
-	}
-	
-	public static void main(String[] args) throws IOException {
-		br = new BufferedReader(new InputStreamReader(System.in));
-		sb = new StringBuilder();
-		char[][] user_map = new char[16][16];
-		
-		for(int tc = 1; tc <= 10; tc++) {
-			br.readLine();
-            for(int i = 0; i < 16; i++) {              
-                user_map[i] = br.readLine().trim().toCharArray();
-            }
-            sb.append("#").append(tc).append(" ").append(bfs(user_map)).append("\n");
 		}
-		System.out.println(sb);
+		return 0;
+		
+	}
+
+	private static void input() throws IOException {
+	
+		String trash = br.readLine().trim(); // 사용 안 함
+		
+		map = new char[16][16];
+		
+		for(int i=0; i<16; i++) {
+			char[] temp = br.readLine().trim().toCharArray();
+			map[i] = temp;
+		}
+//		System.out.println(Arrays.deepToString(map));
 	}
 }
