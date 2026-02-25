e04da26a27dd9712c2397b44a8b66b9a1947f8b5
Choe JinHyeong <wlsgud2414@kangnam.ac.kr>
2026-02-26 08:54:34 +0900

    [D3] Title: 농작물 수확하기, Time: 92 ms, Memory: 27,000 KB -BaekjoonHub

--- SWEA/D3/2805. 농작물 수확하기/농작물 수확하기.java
+++ SWEA/D3/2805. 농작물 수확하기/농작물 수확하기.java
@@ -1,7 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-
+ 
 /**
  * 2805. 농작물 수확하기
  * @author neogul02
@@ -14,18 +14,16 @@ import java.io.InputStreamReader;
  *        [3][1] [3][2] [3][3] 
  *               [4][2]
  * - 각 행 i에 대해, 행마다 마름모의 중심에서의 거리를 계산하여 시작 열과 끝 열을 결정한다.
- * - 중심에서의 거리가 d인 경우, 시작 열은 d, 끝 열은 N-1-d가 된다.
- * - 반복해서 map[i][j]의 값을 더해주면 끝
- * 
+ * - 원이라고 생각하면. 멘헤튼거리로 반지름보다 현 위치가 작으면 마름모라고 생각
  */
 public class Solution {
-
+ 
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static StringBuilder sb = new StringBuilder();
-
+ 
     static int[][] map;
     static int N;
-
+ 
     public static void main(String[] args) throws IOException {
         int T = Integer.parseInt(br.readLine().trim());
         for (int tc = 1; tc <= T; tc++) {
@@ -38,24 +36,30 @@ public class Solution {
     }
     // 마름모 영역 합 계산
     public static int solve() {
-        int mid = N / 2; // 마름모의 중심 행
+        // 마름모의 중심 좌표 구하기
+        int cx = N / 2; // 중심 행
+        int cy = N / 2; // 중심 열
+
+        int r = N / 2; // 반지름 길이
+
         int sum = 0;
-
+        // 모든 배열의 칸을 돌면서, 중심점에서 부터 현재 위치까지 거리가 반지름보다 작으면? -> 마름모 영역
         for (int i = 0; i < N; i++) {
-            int d = Math.abs(mid - i); // 중심에서의 거리
-            int start = d; // 시작 열
-            int end = N - 1 - d; // 끝 열
-
-            for (int j = start; j <= end; j++) {
-                sum += map[i][j];
+            for (int j = 0; j < N; j++) {
+                int distance = Math.abs(i - cx) + Math.abs(j - cy);
+                if (distance <= r) {
+                    sum += map[i][j];
+                    // System.out.printf("거리-> %f , [%d][%d] = %d\n",distance,i,j,map[i][j]);
+                }
             }
         }
+        
         return sum;
     }
-    
+     
     public static void input() throws IOException {
         N = Integer.parseInt(br.readLine().trim());
-
+ 
         map = new int[N][N];
         for (int i = 0; i < N; i++) {
             String temp = br.readLine().trim();
