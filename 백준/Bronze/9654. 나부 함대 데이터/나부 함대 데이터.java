13abb9b8450c0075f96b761aa9e2a5272b4abc80
Choe JinHyeong <wlsgud2414@kangnam.ac.kr>
2026-02-27 08:54:44 +0900

    [Bronze V] Title: 나부 함대 데이터, Time: 64 ms, Memory: 11472 KB -BaekjoonHub

---
+++ 백준/Bronze/9654. 나부 함대 데이터/나부 함대 데이터.java
@@ -0,0 +1,11 @@
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.printf("%-15s%-15s%-11s%-10s\n", "SHIP NAME", "CLASS", "DEPLOYMENT", "IN SERVICE");
+        System.out.printf("%-15s%-15s%-11s%-10d\n", "N2 Bomber ", "Heavy Fighter", "Limited", 21);
+        System.out.printf("%-15s%-15s%-11s%-10d\n", "J-Type 327", "Light Combat", "Unlimited", 1);
+        System.out.printf("%-15s%-15s%-11s%-10d\n", "NX Cruiser ", "Medium Fighter", "Limited", 18);
+        System.out.printf("%-15s%-15s%-11s%-10d\n", "N1 Starfighter", "Medium Fighter", "Unlimited", 25);
+        System.out.printf("%-15s%-15s%-11s%-10d\n", "Royal Cruiser", "Light Combat", "Limited", 4);
+    }
+}
