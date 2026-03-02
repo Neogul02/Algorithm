716f7d7dc42a98f414d53ba7da7905050940f73e
Choe JinHyeong <wlsgud2414@kangnam.ac.kr>
2026-03-03 00:19:50 +0900

    [D6] Title: [S/W 문제해결 응용] 10일차 - 작업순서, Time: 115 ms, Memory: 28,544 KB -BaekjoonHub

--- SWEA/D6/1267. ［S／W 문제해결 응용］ 10일차 － 작업순서/［S／W 문제해결 응용］ 10일차 － 작업순서.java
+++ SWEA/D6/1267. ［S／W 문제해결 응용］ 10일차 － 작업순서/［S／W 문제해결 응용］ 10일차 － 작업순서.java
@@ -18,7 +18,6 @@ public class Solution {
 	static StringBuilder sb = new StringBuilder();
 	static StringTokenizer st;
 	
-	
 	static int V, E; // V: 노드 개수, E: 간선 개수
 	static ArrayList<ArrayList<Integer>> graph;
 	static int[] indegree; // 진입 차수
@@ -36,25 +35,28 @@ public class Solution {
 	private static void wesangSort() {
 		Queue<Integer> queue = new ArrayDeque<>();
 		
-		// 1. 진입차수가 0인 노드를 시작으로 추가
-		for(int i=1; i<=V; i++) {
-			if(indegree[i]==0) {
-				queue.add(i);
-			}
-		}
+        // 1. 진입차수가 0인 노드를 시작으로 추가
+        // 2. 진입차수가 0 이다 = 어떤 노드도 화살표가 꽂히지 않았다 = 가장 먼저 처리해야할 노드이다
+        for(int i=1; i<=V; i++){
+            if(indegree[i] == 0) {
+                queue.add(i); // 진입차수가 0인 노드 큐에 추가
+            }
+        }
 		
-		// 2. 큐가 빌 때까지 반복
-		while(queue.isEmpty()!=true) {
-			int temp = queue.poll();
-			sb.append(temp).append(' ');
-			
-			for(int next : graph.get(temp)) {
-				indegree[next]--; // 현재 노드와 연결된 노드들의 진입차수 1개 줄이기
-				
-				if (indegree[next] == 0) queue.add(next); 
-				
-			}
-		}
+        // 2. 큐가 빌 때까지 반복
+        while (queue.isEmpty() != true) {
+            int temp = queue.poll();
+            sb.append(temp).append(' '); // 처리한 노드 출력
+
+            // 방문 처리를 한 노드의 진입차수를 -1 해주고, 진입차수가 0이 된 노드를 큐에 추가
+            for (int next : graph.get(temp)) {
+                indegree[next]--; // 현재 노드와 연결된 노드들의 진입차수 1개 줄이기
+                if(indegree[next] == 0) {
+                    queue.add(next); // 진입차수가 0이 된 노드 큐에 추가
+                }
+            }
+        }
+		
 	}
 
 	private static void input() throws IOException{
@@ -62,9 +64,8 @@ public class Solution {
 		V = Integer.parseInt(st.nextToken()); 
 		E = Integer.parseInt(st.nextToken());
 		
-		graph = new ArrayList<>();
-		
-		
+        graph = new ArrayList<>();
+        
 		// 0번부터 V번까지 V+1개의 리스트 초기화
 		for(int i=0; i<=V; i++) {
 			graph.add(new ArrayList<>()); // 0번째 인덱스는 0번노드
@@ -73,12 +74,15 @@ public class Solution {
 		indegree = new int[V+1];
 		
 		st = new StringTokenizer(br.readLine().trim()," ");
-		for(int i=1; i<=E; i++) {
-			int from = Integer.parseInt(st.nextToken());
-			int to = Integer.parseInt(st.nextToken());
-			
-			graph.get(from).add(to); // 그래프 -> 저장
-			indegree[to]++; // -> 화살표 꽂히는 쪽에 간선 1개 추가 = 진입 차수 +1
-		}
+        for (int i = 1; i <= E; i++) {
+            int from = Integer.parseInt(st.nextToken());
+            int to = Integer.parseInt(st.nextToken());
+
+            graph.get(from).add(to); // 그래프 -> 저장
+            indegree[to]++; // -> 화살표 꽂히는 쪽에 간선 1개 추가 = 진입 차수 +1
+        }
+        
+        // System.out.println(graph);
+        // System.out.println("indegree: " + java.util.Arrays.toString(indegree));
 	}
 }
