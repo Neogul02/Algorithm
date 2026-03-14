50b3e6d94cf677d207091d8b23c73861cef1ef96
Choe JinHyeong <wlsgud2414@kangnam.ac.kr>
2026-03-14 14:23:00 +0900

    [Bronze II] Title: 기념일 1, Time: 68 ms, Memory: 11764 KB -BaekjoonHub

---
+++ 백준/Bronze/10420. 기념일 1/기념일 1.java
@@ -0,0 +1,65 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // 기념하려는 날
+        int N = Integer.parseInt(br.readLine().trim());
+
+        // 2014년 4월 2일을 사귄날로 시작
+        int year = 2014;
+        int month = 4;
+        int day = 2;
+
+        for (int i = 1; i < N; i++) {
+            day++;
+            // 윤년일때의 2월 
+            if (month == 2) {
+                if (isLeapYear(year)) {
+                    if (day > 29) {
+                        day = 1;
+                        month++;
+                    }
+                } else {
+                    if (day > 28) {
+                        day = 1;
+                        month++;
+                    }
+                }
+            }
+            // 4월, 6월, 9월, 11월은 30일까지
+            else if (month == 4 || month == 6 || month == 9 || month == 11) {
+                if (day > 30) {
+                    day = 1;
+                    month++;
+                }
+            } else {
+                if (day > 31) {
+                    day = 1;
+                    month++;
+                }
+            }
+
+            // 12월이 넘어가면 new year
+            if (month > 12) {
+                month = 1;
+                year++;
+            }
+        }
+        System.out.printf("%d-%02d-%02d", year, month, day);
+    }
+
+    public static boolean isLeapYear(int year) {
+        
+        if (year % 400 == 0)
+            return true;
+        if (year % 100 == 0)
+            return false;
+        if (year % 4 == 0)
+            return true;
+
+        return false;
+    }
+}
