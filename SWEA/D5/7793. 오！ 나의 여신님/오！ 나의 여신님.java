70cfeb56bcb68eec0de5bb3e141bbbf288c09c13
Choe JinHyeong <wlsgud2414@kangnam.ac.kr>
2026-03-15 23:24:09 +0900

    [D5] Title: 오! 나의 여신님, Time: 97 ms, Memory: 26,624 KB -BaekjoonHub

--- SWEA/D5/7793. 오！ 나의 여신님/오！ 나의 여신님.java
+++ SWEA/D5/7793. 오！ 나의 여신님/오！ 나의 여신님.java
@@ -4,175 +4,126 @@ import java.io.InputStreamReader;
 import java.util.ArrayDeque;
 import java.util.StringTokenizer;
 
-/**
- * 7793. 오! 나의 여신님
- * - 악마는 상하좌우 영역을 '부식' 시킨다.. 크큭
- * 
- * - 수연이는 'S', Kamisama는 'D' 는 하나로 fix
- * - 악마는 '*' 돌은 'X', 빈 공간은 '.'
- * 
- * - bfs로 풀이
- * 
- * - 악마는 매 초마다 상하좌우로 부식시킴
- * - 입력 받으면서 각각의 이벤트들의 위치를 저장해두자
- * - 부식이 먼저 일어난 다음에 수연이를 이동시키기
- * 
- * @see bfs
- * 1. 수연이 위치 부터 시작해서 bfs 탐색
- * 2. 매 턴마다 악마를 먼저 확산시키고, 그 다음 수연이를 이동시킨다.
- * 3. 수연이가 카미사마 위치에 도달하면 종료
- */
 public class Solution {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static StringTokenizer st;
     static StringBuilder sb = new StringBuilder();
-    
+
     static int N, M;
     static char[][] map;
     static boolean[][] visited;
-    static int answer;
 
-    static int[] suyeonPos; // 수연이 위치
-    static ArrayDeque<int[]> devilPos; // 악마 위치는 여러개 있을 수 있음
+    static int startX, startY;
+    static ArrayDeque<int[]> devilQueue;
 
-    static int[] dx = { -1, 1, 0, 0 }; // 상하좌우
-    static int[] dy = { 0, 0, -1, 1 };
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException{
         int T = Integer.parseInt(br.readLine().trim());
-        for (int tc = 1; tc <= T; tc++) {
-            // 1. input
+        for(int tc = 1; tc <= T; tc++) {
+            // 1. input()
             input();
 
-            // 2. solve
+            // 2. solve()
             solve(tc);
-        }
 
-        // 3. output
+        }
         System.out.print(sb);
     }
 
-    public static void solve(int tc) {
-        // 1. Init 
-        visited = new boolean[N][M];
-        answer = -1;
+    public static void solve(int tc){
+        ArrayDeque<int[]> SuyeonQueue = new ArrayDeque<>();
+        SuyeonQueue.add(new int[] {startX, startY}); // x좌표, y좌표, 이동 횟수
+        visited[startX][startY] = true;
 
-        // 2. bfs
-        bfs();
+        int time =0;
 
-        // 3. output
-        sb.append("#").append(tc).append(' ');
-        if (answer == -1) {
-            sb.append("GAME OVER");
-        } else {
-            sb.append(answer);
-        }
-        sb.append('\n');
-    }
+        sb.append('#').append(tc).append(' ');
 
-    public static void bfs() {
-        ArrayDeque<int[]> suyeonQueue = new ArrayDeque<>();
-        
-        // 수연이 위치 큐에 추가
-        suyeonQueue.add(new int[] { suyeonPos[0], suyeonPos[1], 0 });
-        visited[suyeonPos[0]][suyeonPos[1]] = true;
+        while(SuyeonQueue.isEmpty()!= true){
+            time ++;
 
-        // 매 턴마다 악마를 먼저 확산시키고, 그 다음 수연이를 이동시킨다.
-        while (suyeonQueue.isEmpty() != true) {
+            // 1. 악마 먼저 이동
+            int devilSize = devilQueue.size();
+            for(int i = 0; i < devilSize; i++) {
+                int[] tempdevil = devilQueue.poll();
+                int devilX = tempdevil[0];
+                int devilY = tempdevil[1];
 
-            // 사이즈 고정
-            int devilSize = devilPos.size();
-            for (int i = 0; i < devilSize; i++) {
-                int[] cur = devilPos.poll();
-                int r = cur[0];
-                int c = cur[1];
+                // 4방향 탐색
+                for(int d=0; d<4; d++){
+                    int nx = devilX + dx[d];
+                    int ny = devilY + dy[d];
 
-                for (int d = 0; d < 4; d++) {
-                    int nr = r + dx[d];
-                    int nc = c + dy[d];
+                    if(nx<0 || ny<0 || nx>=N || ny>=M) continue;
 
-                    // 범위 밖이면 확산 불가
-                    if (nr < 0 || nr >= N || nc < 0 || nc >= M) {
-                        continue;
-                    }
+                    if(map[nx][ny]=='D' || map[nx][ny]=='X' || map[nx][ny]=='*') continue;
 
-                    // 이미 부식시킨곳이거나 돌이거나 카미사마 위치는 부식 불가
-                    if (map[nr][nc] == '*' || map[nr][nc] == 'X' || map[nr][nc] == 'D') {
-                        continue;
-                    }
+                    map[nx][ny] = '*';
+                    devilQueue.add(new int[] {nx, ny});
 
-                    // 빈 공간이랑 수연이 위치로는 전파 가능
-                    if (map[nr][nc] == '.' || map[nr][nc] == 'S') {
-                        map[nr][nc] = '*'; // 전파 처리
-                        devilPos.add(new int[] { nr, nc });
-                    }
                 }
             }
 
-            // 사이즈 고정
-            int suyeonSize = suyeonQueue.size();
-            for (int i = 0; i < suyeonSize; i++) {
-                int[] cur = suyeonQueue.poll();
-                int r = cur[0];
-                int c = cur[1];
-                int time = cur[2];
+            // 2. 수연 이동
+            int suyeonSize = SuyeonQueue.size();
+            for(int i=0; i< suyeonSize; i++){
+                int[] tempSuyeon = SuyeonQueue.poll();
+                int suyeonX = tempSuyeon[0];
+                int suyeonY = tempSuyeon[1];
 
-                for (int d = 0; d < 4; d++) {
-                    int nr = r + dx[d];
-                    int nc = c + dy[d];
+                for(int d=0; d<4; d++){
+                    int nx = suyeonX + dx[d];
+                    int ny = suyeonY + dy[d];
 
-                    // 범위 밖이면 이동 불가
-                    if (nr < 0 || nr >= N || nc < 0 || nc >= M) {
-                        continue;
-                    }
+                    if(nx<0 || ny<0 || nx>=N || ny>=M) continue; 
 
-                    // 카미사마 위치면 종료
-                    if (map[nr][nc] == 'D') {
-                        answer = time + 1;
+                    if(map[nx][ny] == 'D'){
+                        sb.append(time).append('\n');
                         return;
                     }
+                    
+                    if(visited[nx][ny] == true) continue;
 
-                    // 이미 방문한곳은 스킵
-                    if (visited[nr][nc]) {
-                        continue;
-                    }
+                    if(map[nx][ny] == '*' || map[nx][ny] =='X') continue;
 
-                    // 악마가 부식시킨 곳이거나 돌이 있는 곳은 이동 불가
-                    if (map[nr][nc] == '*' || map[nr][nc] == 'X') {
-                        continue;
-                    }
-
-                    // 이동할 수 있는 곳이면 방문처리하고 큐에 추가
-                    if (map[nr][nc] == '.') {
-                        visited[nr][nc] = true;
-                        suyeonQueue.add(new int[] { nr, nc, time + 1 });
-                    }
+                    map[nx][ny] = 'S';
+                    visited[nx][ny] = true;
+                    SuyeonQueue.add(new int[] {nx, ny});
+                    
                 }
             }
         }
+
+        // 모든 위치 끝인데 return 안됨 = gameover 
+        sb.append("GAME OVER").append('\n');
+        return;
     }
-    
-    public static void input() throws IOException {
-        st = new StringTokenizer(br.readLine().trim());
+
+    public static void input() throws IOException{
+        st = new StringTokenizer(br.readLine().trim(), " ");
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-
         map = new char[N][M];
-        devilPos = new ArrayDeque<>();
-        for (int i = 0; i < N; i++) {
-            String line = br.readLine().trim();
-            for (int j = 0; j < M; j++) {
-                map[i][j] = line.charAt(j);
-                // 수연이 위치
-                if(map[i][j] == 'S') {
-                    suyeonPos = new int[] { i, j };
-                }
+        visited = new boolean[N][M];
 
-                // 악마 위치는 여러개 있을 수 있음
-                if(map[i][j] == '*') {
-                    devilPos.add(new int[] { i, j });
+        devilQueue = new ArrayDeque<>();
+
+        for(int i = 0; i < N; i++) {
+            String line = br.readLine().trim();
+            for(int j = 0; j < M; j++) {
+                map[i][j] = line.charAt(j);
+
+                if(map[i][j] == 'S') {
+                    startX = i;
+                    startY = j;
+                } else if(map[i][j] == '*') {
+                    devilQueue.add(new int[] {i, j});
                 }
             }
         }
     }
-} 
+}
+    
+
