66371962d8e0a6b6523910bed10194c3e442783a
Choe JinHyeong <wlsgud2414@kangnam.ac.kr>
2026-03-16 14:01:17 +0900

    [D3] Title: [Professional] 조합, Time: 105 ms, Memory: 33,536 KB -BaekjoonHub

---
+++ SWEA/D3/5607. ［Professional］ 조합/README.md
@@ -0,0 +1,15 @@
+# [D3] [Professional] 조합 - 5607 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AWXGKdbqczEDFAUo) 
+
+### 성능 요약
+
+메모리: 33,536 KB, 시간: 105 ms, 코드길이: 2,539 Bytes
+
+### 제출 일자
+
+2026-03-16 14:01
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D3/5607. ［Professional］ 조합/［Professional］ 조합.java
@@ -0,0 +1,90 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+/**
+ * 5607. 조합
+ * @author neogul02
+ * 
+ * 페르마의 소정리 = a^(p-1) ≡ 1 (mod p) -> a^(p-2) ≡ a^(-1) (mod p)
+ */
+public class Solution {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+    static StringTokenizer st;
+
+    static int N, R;
+    static final int MOD = 1234567891;
+
+    static long[] factorial; // 팩토리얼 저장 배열
+
+    public static void main(String[] args) throws IOException {
+        int T = Integer.parseInt(br.readLine().trim());
+        initFactorials(1000000);
+        for (int tc = 1; tc <= T; tc++) {
+            input();
+            solve(tc);
+            
+        }
+        System.out.print(sb);
+    }
+
+    public static void initFactorials(int max) {
+        // 팩토리얼을 미리 계산하여 저장
+        factorial = new long[max + 1];
+        factorial[0] = 1; // 0! = 1;
+        
+        for (int i = 1; i <= max; i++) {
+            factorial[i] = (factorial[i - 1] * i) % MOD;
+        }
+
+        // System.out.println(Arrays.toString(factorial));
+    }
+
+    // 조합 공식 nCr = n! / (r! * (n-r)!)
+    // n! * (r! * (n-r)!)^-1
+    // n! * (r!)^-1 * ((n-r)!)^-1
+    public static void solve(int tc){
+        long result = 0;
+
+        long nFac = factorial[N];
+        long rFac = factorial[R];
+        long nMinusR = factorial[N - R];
+
+        // (r! * (n-r)!)^-1 계산
+        // 페르마의 소정리 = a^(p-1) ≡ 1 (mod p) -> a^(p-2) ≡ a^(-1) (mod p)
+        long reverseRfac = pow(rFac, MOD -2) % MOD;
+        long reverseNMinusRfac = pow(nMinusR, MOD -2) % MOD;
+
+        result = nFac * reverseRfac % MOD;
+        result = result * reverseNMinusRfac % MOD;
+
+        sb.append("#").append(tc).append(" ")
+            .append(result).append("\n");
+    }
+
+    //
+    private static long pow(long base, int exp) {
+
+        long result = 1;
+        long cur = base;
+
+        while (exp > 0) {
+            if ((exp & 1)==1) { // exp가 홀수면
+                result = (result * cur) % MOD; // result에 cur 곱하기
+
+            }
+            cur = (cur * cur) % MOD; // cur 제곱하기
+            exp >>= 1; // exp를 오른쪽으로 한 비트 시프트 (2로 나누기)
+
+        }
+        return result;
+
+    }
+
+    public static void input() throws IOException{
+        st = new StringTokenizer(br.readLine().trim(), " ");
+        N = Integer.parseInt(st.nextToken());
+        R = Integer.parseInt(st.nextToken());
+    }
+}
