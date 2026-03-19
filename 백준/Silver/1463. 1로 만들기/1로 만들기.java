ba0526c1609b842217155f3db6008d0a02fc2a67
Choe JinHyeong <wlsgud2414@kangnam.ac.kr>
2026-03-19 17:15:07 +0900

    [Silver III] Title: 1로 만들기, Time: 96 ms, Memory: 15468 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -21,12 +21,12 @@ public class Main {
         dp[1] = 0;
 
         for(int i = 2; i <= N; i++){
-            dp[i] = dp[i - 1] + 1;
-            if(i % 2 == 0){
-                dp[i] = Math.min(dp[i], dp[i / 2] + 1);
+            dp[i]  = dp[i - 1] + 1;
+            if(i%2 == 0){
+                dp[i] = Math.min(dp[i/2]+1, dp[i]);
             }
-            if(i % 3 == 0){
-                dp[i] = Math.min(dp[i], dp[i / 3] + 1);
+            if(i%3 == 0){    
+                dp[i] = Math.min(dp[i/3]+1, dp[i]);
             }
         }
 
