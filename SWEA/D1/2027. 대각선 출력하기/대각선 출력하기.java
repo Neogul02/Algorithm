c152232086725ac9b57a9e5a0e344229f7645fea
Choe JinHyeong <wlsgud2414@kangnam.ac.kr>
2026-03-31 16:17:15 +0900

    [D1] Title: 대각선 출력하기, Time: 80 ms, Memory: 25,472 KB -BaekjoonHub

--- SWEA/D1/2027. 대각선 출력하기/대각선 출력하기.java
+++ SWEA/D1/2027. 대각선 출력하기/대각선 출력하기.java
@@ -4,11 +4,16 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 public class Solution {
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-  	static StringBuilder sb = new StringBuilder();
+  static StringBuilder sb = new StringBuilder();
 	static StringTokenizer st;
-    
 	public static void main(String[] args) throws IOException {
-		sb.append("#++++\n+#+++\n++#++\n+++#+\n++++#");
-        System.out.print(sb);
+		for(int i=0; i<5; i++){
+			for(int j=0; j<5; j++){
+				if(i==j) sb.append("#");
+				else sb.append("+");
+			}
+			sb.append("\n");
+		}
+		System.out.print(sb);
 	}
 }
