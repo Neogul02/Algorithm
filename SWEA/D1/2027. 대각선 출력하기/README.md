4aab238850189bb2ee9e1ffd700a0a15fd9699c8
Choe JinHyeong <wlsgud2414@kangnam.ac.kr>
2026-03-31 16:12:12 +0900

    [D1] Title: 대각선 출력하기, Time: 79 ms, Memory: 21,760 KB -BaekjoonHub

---
+++ SWEA/D1/2027. 대각선 출력하기/README.md
@@ -0,0 +1,15 @@
+# [D1] 대각선 출력하기 - 2027 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV5QFuZ6As0DFAUq) 
+
+### 성능 요약
+
+메모리: 21,760 KB, 시간: 79 ms, 코드길이: 461 Bytes
+
+### 제출 일자
+
+2026-03-31 16:12
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D1/2027. 대각선 출력하기/대각선 출력하기.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+public class Solution {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+  	static StringBuilder sb = new StringBuilder();
+	static StringTokenizer st;
+    
+	public static void main(String[] args) throws IOException {
+		sb.append("#++++\n+#+++\n++#++\n+++#+\n++++#");
+        System.out.print(sb);
+	}
+}
