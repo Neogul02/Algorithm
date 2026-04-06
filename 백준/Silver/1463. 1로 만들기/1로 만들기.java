a3fe7d5bae5c5eb6761c985c6e5113074e025951
Choe JinHyeong <wlsgud2414@kangnam.ac.kr>
2026-04-06 14:12:53 +0900

    [Silver III] Title: 1로 만들기, Time: 100 ms, Memory: 15692 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -3,33 +3,36 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
+/**
+ * 연산횟수의 최솟값을 구해야함
+ * 1. 더하는건 그냥 이전 숫자에서 +1 한 번 추가
+ * 2. i/2 위치에서 *2 하면 i 니까 [i/2] + 1
+ * 3. i/3 위치에서 *3 하면 i 니까 [i/3] + 1
+ */
 public class Main {
+  static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+  static StringTokenizer st;
+  static StringBuilder sb = new StringBuilder();
 
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static StringTokenizer st;
-    static StringBuilder sb = new StringBuilder();
+  static int N;
+  static int[] dp;
+  public static void main(String[] args) throws IOException{
+    N = Integer.parseInt(br.readLine().trim());
 
-    static int N;
+    dp = new int[N+1];
+    dp[1] = 0;
     
+    for(int i=2; i<N+1; i++){
+      // 1을 더하는 경우
+      dp[i] = dp[i-1] + 1;
+      
+      // 2로 나누는 경우
+      if(i%2==0) dp[i] = Math.min(dp[i], dp[i/2]+1);
 
-    static int result;
-
-    public static void main(String[] args) throws IOException{
-        N = Integer.parseInt(br.readLine().trim());
-
-        int dp[] = new int[N + 1];
-        dp[1] = 0;
-
-        for(int i = 2; i <= N; i++){
-            dp[i]  = dp[i - 1] + 1;
-            if(i%2 == 0){
-                dp[i] = Math.min(dp[i/2]+1, dp[i]);
-            }
-            if(i%3 == 0){    
-                dp[i] = Math.min(dp[i/3]+1, dp[i]);
-            }
-        }
-
-        System.out.println(dp[N]);
+      // 3으로 나누는 경우
+      if(i%3==0) dp[i] = Math.min(dp[i], dp[i/3]+1);
     }
+
+    System.out.print(dp[N]);
+  }
 }
