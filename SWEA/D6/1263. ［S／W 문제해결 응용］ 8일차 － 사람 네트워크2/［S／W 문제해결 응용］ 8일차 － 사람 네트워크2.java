4d1767112b2421a6017d872616a4e3e14ae27ef4
Choe JinHyeong <wlsgud2414@kangnam.ac.kr>
2026-04-14 14:45:03 +0900

    [D6] Title: [S/W 문제해결 응용] 8일차 - 사람 네트워크2, Time: 2,304 ms, Memory: 107,300 KB -BaekjoonHub

--- SWEA/D6/1263. ［S／W 문제해결 응용］ 8일차 － 사람 네트워크2/［S／W 문제해결 응용］ 8일차 － 사람 네트워크2.java
+++ SWEA/D6/1263. ［S／W 문제해결 응용］ 8일차 － 사람 네트워크2/［S／W 문제해결 응용］ 8일차 － 사람 네트워크2.java
@@ -1,16 +1,14 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayDeque;
 import java.util.StringTokenizer;
 
 /**
  * 1263. 사람 네트워크 2
  * 
  * @author neogul02
- *
- *         - 간선의 가중치를 1로 생각하고 플로이드 워셜 알고리즘으로 각 노드마다 가중치를 계산함.
- *         - 가장 작은 가중치를 가진 노드가 가장 가까운 사람이 되고
- *         - 가장 작은 가중치를 출력함
+ *         - 그래프를 초기화 하고 각 노드에서 bfs를 진행해서 가장 작은 가중치를 출력
  */
 public class Solution {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -28,54 +26,54 @@ public class Solution {
         for (int tc = 1; tc <= T; tc++) {
             sb.append("#").append(tc).append(" ");
             input();
-            floydWarshall();
+            bfs();
         }
         System.out.print(sb);
     }
 
-    public static void floydWarshall() {
-        // 경출도
-        for (int k = 0; k < N; k++) { // k는 경로 노드
-            for (int i = 0; i < N; i++) { // i는 출발 노드
-                for (int j = 0; j < N; j++) { // j는 도착 노드
+    public static void bfs() {
+        ArrayDeque<int[]> queue = new ArrayDeque<>();
+        int min = INF;
 
-                    // i에서 k로 가고, k에서 j로 갈 수 있다면 i에서 j로 갈 수 있다
-                    if (graph[i][k] != INF && graph[k][j] != INF) {
-                        graph[i][j] = Math.min(graph[i][j], graph[i][k] + graph[k][j]);
+        for (int i = 0; i < N; i++) {
+            queue.offer(new int[] { i, 0 }); // 각 노드에서 시작해서 가중치 0으로 시작
+            boolean[] visited = new boolean[N]; // 방문처리 배열도 각 노드마다 초기화
+            visited[i] = true;
+            int sum = 0;
+
+            while (!queue.isEmpty()) {
+                int[] temp = queue.poll();
+                int node = temp[0];
+                int weight = temp[1];
+
+                sum += weight;
+
+                for (int j = 0; j < N; j++) {
+                    if (!visited[j] && graph[node][j] != INF) {
+                        visited[j] = true;
+                        queue.offer(new int[] { j, graph[node][j] + weight });
                     }
                 }
             }
+            min = Math.min(min, sum);
         }
-
-        int minSum = Integer.MAX_VALUE;
-        for (int i = 0; i < N; i++) {
-            int sum = 0;
-            for (int j = 0; j < N; j++) {
-                sum += graph[i][j];
-            }
-            minSum = Math.min(minSum, sum);
-        }
-
-        sb.append(minSum).append("\n");
+        sb.append(min).append("\n");
     }
 
     public static void input() throws IOException {
         st = new StringTokenizer(br.readLine().trim());
         N = Integer.parseInt(st.nextToken());
-
         graph = new int[N][N];
-
-        // row-by-row 인접행렬
         for (int i = 0; i < N; i++) {
-            graph[i] = new int[N];
             for (int j = 0; j < N; j++) {
-                int value = Integer.parseInt(st.nextToken());
+                int temp = Integer.parseInt(st.nextToken());
+
                 if (i == j) {
-                    graph[i][j] = 0; // 자기 자신으로의 거리는 0
-                } else if (value == 1) {
-                    graph[i][j] = 1; // 연결된 간선의 가중치는 1
+                    graph[i][j] = 0;
+                } else if (temp == 1) {
+                    graph[i][j] = 1;
                 } else {
-                    graph[i][j] = INF; // 연결되지 않은 간선은 INF
+                    graph[i][j] = INF;
                 }
             }
         }
