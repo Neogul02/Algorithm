532f00be957c2b107c4c58c1bafb2ffebf0acd60
Choe JinHyeong <wlsgud2414@kangnam.ac.kr>
2026-04-24 17:47:59 +0900

    [level 0] Title: 주사위 게임 1, Time: 0.04 ms, Memory: 91 MB -BaekjoonHub

---
+++ 프로그래머스/0/181839. 주사위 게임 1/주사위 게임 1.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int a, int b) {
+        if(a%2==1 && b%2==1){
+            return a*a + b*b;
+        }
+        
+        if(a%2==0 && b%2==0){
+            return Math.abs(a-b);
+        }
+        
+        return 2*(a+b);
+    }
+}
